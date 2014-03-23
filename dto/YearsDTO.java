be7a495b3a1117c93331e203f90d4919f297bcee
Marvin Yormie <cmarviny1@gmail.com>
2014-03-22 23:45:28 -0400

    Create YearsDTO.java

---
+++ dto/YearsDTO.java
@@ -0,0 +1,49 @@
+package org.celts.db.dto;
+
+public class YearsDTO {
+
+	
+	private  Integer id;
+	private int yr;
+	private int yrofdeptr;
+	private int yrpartnered;
+	private int yrptcpd;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+
+	public int getYr() {
+		return yr;
+	}
+	public void setYr(int yr) {
+		this.yr =yr;
+	}
+	
+	public int getYrofdeptr() {
+		return yrofdeptr;
+	}
+	public void setYrofdeptr(int yrofdeptr) {
+		this.yrofdeptr=yrofdeptr;
+	}
+
+	
+	public int getYrpartnered() {
+		return yrpartnered;
+	}
+	public void setYrpartnered(int yrpartnered) {
+		this.yrpartnered = yrpartnered;
+	}
+	
+	
+	public int getYrptcpd() {
+		return yrptcpd;
+	}
+	public void setYrptcpd(int yrptcpd) {
+		this.yrptcpd = yrptcpd;
+	}
+}
